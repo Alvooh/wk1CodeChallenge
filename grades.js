fa835be800a2c03681dcfe94eaf903492fb59420
AlvinObala <alvinomolo99@gmail.com>
2024-03-24 20:34:53 +0300

    add comments to my javascript files

--- grades.js
+++ grades.js
@@ -1,12 +1,20 @@
+// Define a function named gradeGenerator that takes a numerical mark as input
 function gradeGenerator (mark){
     if (mark > 79) {
         return 'A';
+    // If the mark is between 60 and 79 (inclusive), return 'B'
     } else if (mark >= 60 && mark <= 79) {
         return 'B';
+
+    // If the mark is between 50 and 59 (inclusive), return 'C'
     } else if (mark >= 50 && mark <= 59) {
         return 'C';
+
+    // If the mark is between 40 and 49 (inclusive), return 'D'
     } else if (mark >= 40 && mark <= 49) {
         return 'D';
+    
+    // If none of the above conditions are met, return 'E'
     } else {
         return 'E';
     }
@@ -23,7 +31,7 @@ function promptGrade() {
         // If the input is invalid, display an error message and prompt the user again
         alert('Invalid Input! Enter values between 0 and 100');
 
-    
+        // Call the function again to prompt the user
         promptGrade(); 
         return; // Exit the function
     }
@@ -37,3 +45,7 @@ function promptGrade() {
 
 // Call the promptGrade function to start the program
 promptGrade()
+
+
+
+//the code above works from the front end since node doesnt allow prompt function 
--- speed.js
+++ speed.js
@@ -1,25 +1,28 @@
+//function that calculates the demerit points of drivers driving above speed limit 
 function calculateDemeritPoints(speed) {
     const speedLimit = 70;
     let demeritPoints = 0;
-
+//if statement to check if driver is driving below the speed limit 
     if (speed <= speedLimit) {
         console.log("Ok");
     } else {
+        //calculates demerit points and uses the Math.floor fuction to ensure no decimal points 
         demeritPoints = Math.floor((speed - speedLimit) / 5);
         console.log("Demerit points: " + demeritPoints);
     }
 
     return demeritPoints;
 }
-
+//function that prompts user to enter speed driver is travelling at 
 function promptSpeed() {
     const speed = prompt("Enter the speed of the car (in km/h):");
+    //calls the calculteDemeritPoints function and inputs speed entered by user as the argument 
     const demeritPoints = calculateDemeritPoints(speed);
     alert (`your demerit points is ${demeritPoints}`)
     return demeritPoints
    
 }
-
+//calls the function promptSpeed()
 promptSpeed();
 
 
